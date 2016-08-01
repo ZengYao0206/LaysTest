b1ed6251dc1a027ab75cd19dd4aeac972f9b42a5
zengyao <zengyao@111.com.cn>
2016-08-01 11:12:58 +0800

    添加CocoasPod

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0'
+target "GraphicsTest" do 
+pod 'MBProgressHUD', '~> 0.8'
+end
---
+++ Pods/Target Support Files/Pods-GraphicsTest/Pods-GraphicsTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GraphicsTest : NSObject
+@end
+@implementation PodsDummy_Pods_GraphicsTest
+@end
